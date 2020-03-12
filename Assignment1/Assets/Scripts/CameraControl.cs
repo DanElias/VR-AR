c3c42458047dce8a7c487d7f17e0d48f3f413722
Dan <A01208905@itesm.mx>
2020-03-12 16:36:01 +0800

    [DAN] Initial commit

---
+++ Assignment1/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start(){
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    /*The LateUpdate() is to guarantee that all the 
+    update has been done in each frame and then update 
+    this function.*/
+    void LateUpdate(){
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assignment1/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    public GameObject explosion;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate(){
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("PickUp")){
+            GameObject explosionClone = Instantiate(explosion, other.gameObject.transform.position, other.gameObject.transform.rotation);
+            other.gameObject.SetActive(false);
+            Destroy(other.gameObject);
+        }
+    }
+}
